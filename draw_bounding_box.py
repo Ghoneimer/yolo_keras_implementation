429c09bbd4415abbd1f4b245bd1f39ea7ef35880
Mohammed Hussen <35658538+MohamedHussen1994@users.noreply.github.com>
2018-02-25 16:35:03 +0200

    Create draw_bounding_box.py

---
+++ draw_bounding_box.py
@@ -0,0 +1,51 @@
+import colorsys
+import imghdr
+import os
+import random
+import numpy as np
+from PIL import Image, ImageDraw, ImageFont
+import cv2
+import matplotlib.pyplot as plt
+
+def generate_colors(class_names):
+    hsv_tuples = [(x / len(class_names), 1., 1.) for x in range(len(class_names))]
+    colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
+    colors = list(map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)), colors))
+    random.seed(10101)  # Fixed seed for consistent colors across runs.
+    random.shuffle(colors)  # Shuffle colors to decorrelate adjacent classes.
+    random.seed(None)  # Reset seed to default.
+    return colors
+
+def draw_boxes(image, out_scores, out_boxes, 
+               out_classes, # the output of the yolo ex if it detect cat class for 2 object [1,1]
+               class_names, # the name of all the classes used in the dataset ex for 2 classes ["cat","dog"]
+               colors):
+    
+    font = ImageFont.truetype(font="arial.ttf",
+                              size=25)
+    thickness = 5
+
+    for i, c in reversed(list(enumerate(out_classes))):
+        predicted_class = class_names[c]
+        box = out_boxes[i]
+        score = out_scores[i]
+
+        label = '{} {:.2f}'.format(predicted_class, score)
+
+        draw = ImageDraw.Draw(image)
+        label_size = draw.textsize(label, font)
+
+        left, top, right, bottom = box
+        print(label, (left, top), (right, bottom))
+
+        if top - label_size[1] >= 0:
+            text_origin = np.array([left, top - label_size[1]])
+        else:
+            text_origin = np.array([left, top + 1])
+
+        # My kingdom for a good redistributable image drawing library.
+        for i in range(thickness):
+            draw.rectangle([left + i, top + i, right - i, bottom - i], outline=colors[c])
+        draw.rectangle([tuple(text_origin), tuple(text_origin + label_size)], fill=colors[c])
+        draw.text(text_origin, label, fill=(0, 0, 0), font=font)
+        del draw
